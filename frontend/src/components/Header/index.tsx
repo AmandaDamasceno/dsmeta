c8a85e009ea265eeb1517036b061807ffe9ffcf9
Amanda Damasceno <amandamasceno.rocha@gmail.com>
2022-07-13 20:34:59 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="DSMeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://github.com/AmandaDamasceno" target="_blank">
+            Amanda Damasceno
+          </a>
+        </p>
+      </div>
+    </header>
+  )
+}
+
+export default Header
